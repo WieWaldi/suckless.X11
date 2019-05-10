f4b7529a5cd53792b3de2bc7b0564e25f9cb818c
Waldemar Schroeer <w@ldemar.de>
2019-05-10 22:00:08 +0200

    Work done.

--- setup.sh
+++ setup.sh
@@ -16,26 +16,23 @@ do
     read antwoord
     case $antwoord in
         [yY] | [yY][Ee][Ss] )
+            printf "\n Preparing CentOS 7.\n"
+            sudo yum install -y xorg-x11-xinit xorg-x11-apps xorg-x11-xbitmaps xorg-x11-utils xorg-x11-drv-evdev xorg-x11-fonts-misc.noarch libXrandr-devel libX11-devel libXft-devel libXinerama-devel xterm imsettings ncurses-term ncurses-devel webkitgtk4-devel glib2-devel gcr-devel mesa-libegl mesa-libgl mesa-dri-drivers
             printf "\n Installing Desktop and Suckless Tools to your home directory.\n"
             declare -a Xres=( ".Xresources" ".xinitrc" ".slocktext" ".fonts" )
             for i in "${Xres[@]}"
             do
-                cp ${i} ~
+                cp ${cdir}/${i} ~
             done
-
             /bin/fc-cache
             /bin/fc-list
             sleep 2
-
             declare -a sucklesstools=( "dwm" "dmenu" "st" "slock" "surf" "tabbed" )
             for i in "${sucklesstools[@]}"
             do
                 cd ${cdir}/${i}
                 $make clean install
             done
-
-
-
             break
             ;;
         [nN] | [n|N][O|o] )
