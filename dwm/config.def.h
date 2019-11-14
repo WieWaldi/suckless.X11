5f44e1cae6a5e537d8b7d4152e9888def5bc08c6
Waldemar Schroeer <lassmichinruhe@rz-amper.de>
2019-11-14 21:35:05 +0100

    Work done.

--- dwm/config.def.h
+++ dwm/config.def.h
@@ -1,8 +1,8 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx      = 3;        /* border pixel of windows */
-static const unsigned int gappx         = 7;        /* gaps between windows */
+static const unsigned int borderpx      = 2;        /* border pixel of windows */
+static const unsigned int gappx         = 2;        /* gaps between windows */
 static const unsigned int snap          = 0;       /* snap pixel */
 static const int showbar                = 1;        /* 0 means no bar */
 static const int topbar                 = 1;        /* 0 means bottom bar */
