e0f262cd6774217d1b8876b556a86e9705c64757
Waldemar Schroeer <lassmichinruhe@rz-amper.de>
2019-12-02 18:45:25 +0100

    setup.sh rewritten.

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/usr/bin/env bash
 #
 # +-------------------------------------------------------------------------+
 # | Setup for suckless.X11 tools                                            |
@@ -7,35 +7,53 @@
 # |                       lassmichinruhe@rz-amper.de                        |
 # +-------------------------------------------------------------------------+
 
+declare -a Xres=( ".Xresources" ".xinitrc" ".slocktext" )
+declare -a sucklesstools=( "dwm" "dmenu" "st" "slock" "surf" "tabbed" )
 make=/bin/make        
 cdir=$(pwd)
 
-while :
-do
-    printf "\n\n Install Desktop and Suckless Tolls? >> "
+function Display_Warning() {
+    /bin/clear
+    /bin/cat ${cdir}/setup-warning.txt
+}
+
+function Install_X11files() {
+    /bin/mkdir -p ~/tmp
+    for i in "${Xres[@]}"
+    do
+        /bin/cp -r ${cdir}/${i} ~
+    done
+    /bin/cp -r ${cdir}/.local/share/fonts ~/.local/share
+    /bin/fc-cache
+}
+
+function Install_Suckless() {
+    for i in "${sucklesstools[@]}"
+    do
+        cd ${cdir}/${i}
+        $make install clean
+    done
+}
+
+function make_clean() {
+    printf "\n Cleaning up.\n"
+    for i in "${sucklesstools[@]}"
+    do
+        cd ${cdir}/${i}
+        $make clean
+    done
+}
+
+while true; do
+    printf "\n\n Go ahead? (Yes|No) >> "
     read antwoord
     case $antwoord in
         [yY] | [yY][Ee][Ss] )
-            printf "\n Creating temp directory in your home directory.\n"
-            /bin/mkdir -p ~/tmp
-            # printf "\n Preparing CentOS 7.\n"
-            # sudo yum install -y xorg-x11-xinit xorg-x11-apps xorg-x11-xbitmaps xorg-x11-utils xorg-x11-drv-evdev xorg-x11-fonts-misc.noarch libXrandr-devel libX11-devel libXft-devel libXinerama-devel xterm imsettings ncurses-term ncurses-devel webkitgtk4-devel glib2-devel gcr-devel mesa-libegl mesa-libgl mesa-dri-drivers
-            printf "\n Installing Desktop and Suckless Tools to your home directory.\n"
-            declare -a Xres=( ".Xresources" ".xinitrc" ".slocktext" )
-            for i in "${Xres[@]}"
-            do
-                /bin/cp -r ${cdir}/${i} ~
-            done
-            /bin/cp -r ${cdir}/.local/share/fonts ~/.local/share
-            /bin/fc-cache
-            /bin/fc-list
-            sleep 2
-            declare -a sucklesstools=( "dwm" "dmenu" "st" "st.alpha" "slock" "surf" "tabbed" )
-            for i in "${sucklesstools[@]}"
-            do
-                cd ${cdir}/${i}
-                $make install clean
-            done
+            Display_Warning
+            Install_X11files
+            Install_Suckless
+            make_clean
+            printf "\n I'm done\n\n"
             break
             ;;
         [nN] | [n|N][O|o] )
@@ -43,34 +61,8 @@ do
             break
             ;;
         *)
-            printf "\n Wut?\n\n"
+            printf "  Wut?"
     esac
 done
 
-while :
-do
-    printf "\n\n Make Clean? >> "
-    read antwoord
-    case $antwoord in
-        [yY] | [yY][Ee][Ss] )
-            printf "\n Cleaning up.\n"
-            declare -a sucklesstools=( "dwm" "dmenu" "st" "slock" "surf" "tabbed" )
-            for i in "${sucklesstools[@]}"
-            do
-                cd ${cdir}/${i}
-                $make clean
-            done
-            break
-            ;;
-        [nN] | [n|N][O|o] )
-            printf "\n Oh Boy, you should reconsider your decision."
-            break
-            ;;
-        *)
-            printf "\n Wut?\n\n"
-    esac
-
-done
-
-printf "\n\n I'm done\n\n"
 exit 0
