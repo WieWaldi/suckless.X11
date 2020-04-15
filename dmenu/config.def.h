962b55d7b55bc6cbfe956d417f6be950bebf8a73
Waldemar Schroeer <wiewaldi@idun.rz-amper.de>
2020-04-16 00:05:23 +0200

    minimum width changed to 900

--- dmenu/config.def.h
+++ dmenu/config.def.h
@@ -3,7 +3,7 @@
 
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
 static int centered = 1;                    /* -c option; centers dmenu on screen */
-static int min_width = 500;                    /* minimum width when centered */
+static int min_width = 900;                    /* minimum width when centered */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
 	"monospace:size=10"
