3db72c5fc20b42226a2fdd213f41367d5eefe7ec
Waldemar Schroeer <wiewaldi@idun.rz-amper.de>
2020-04-15 22:41:54 +0200

    Changed mouse cursor to hand

--- st/config.def.h
+++ st/config.def.h
@@ -141,7 +141,8 @@ static unsigned int rows = 24;
 /*
  * Default colour and shape of the mouse cursor
  */
-static unsigned int mouseshape = XC_xterm;
+/* static unsigned int mouseshape = XC_xterm; */
+static unsigned int mouseshape = XC_hand2;
 static unsigned int mousefg = 7;
 static unsigned int mousebg = 0;
 
