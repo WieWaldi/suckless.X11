8a03eed9ce634aea37bd09cf152b97208710e45b
Waldemar Schroeer <wiewaldi@idun.rz-amper.de>
2020-04-17 22:33:27 +0200

    prepared for slop and maim

--- prepare.sh
+++ prepare.sh
@@ -19,28 +19,26 @@ function Copy_Files {
 }
 
 function Install_CentOS_7 {
-    yum install -y vim git wget ftp make automake gcc gcc-c++ kernel-devel patch net-tools bind-utils epel-release asciidoc
+    yum install -y vim git wget ftp make cmake automake gcc gcc-c++ kernel-devel kernel-headers patch net-tools bind-utils epel-release asciidoc perl
     yum install -y xorg-x11-xinit xorg-x11-apps xorg-x11-xbitmaps xorg-x11-utils xterm xclip
     yum install -y xorg-x11-drv-evdev xorg-x11-drv-synaptics xorg-x11-fonts-misc.noarch libXrandr-devel libX11-devel libXft-devel libXScrnSaver-devel libcurl-devel libXt-devel
-    yum install -y libXinerama-devel imsettings ncurses-term ncurses-devel imlib2-devel libexif-devel giflib-devel
+    yum install -y libXinerama-devel imsettings ncurses-term ncurses-devel imlib2-devel libexif-devel giflib-devel glm-devel glew-devel
     yum install -y gcr-devel mesa-libEGL mesa-libGL mesa-dri-drivers dbus-x11 dbus-devel
     yum install -y webkitgtk4-devel glib2-devel gcr-devel mesa-dri-drivers xorg-x11-utils libconfig-devel pango-devel gtk2-devel
     yum install -y "gstreamer*"
     yum install -y ntp ntpdate
-    yum install -y kernel-devel kernel-headers gcc make perl
 }
 
 function Install_CentOS_8() {
     dnf config-manager --enable PowerTools
-    yum install -y vim git wget ftp make automake gcc gcc-c++ kernel-devel patch net-tools bind-utils epel-release asciidoc
-    yum install -y xorg-x11-xinit xorg-x11-apps xorg-x11-xbitmaps xorg-x11-utils xterm xclip
-    yum install -y xorg-x11-drv-evdev xorg-x11-drv-synaptics xorg-x11-fonts-misc.noarch libXrandr-devel libX11-devel libXft-devel libXScrnSaver-devel libcurl-devel libXt-devel
-    yum install -y libXinerama-devel imsettings ncurses-term ncurses-devel imlib2-devel libexif-devel giflib-devel
-    yum install -y gcr-devel mesa-libEGL mesa-libGL mesa-dri-drivers dbus-x11 dbus-devel
-    yum install -y webkitgtk4-devel glib2-devel gcr-devel mesa-dri-drivers xorg-x11-utils libconfig-devel pango-devel gtk2-devel
-    yum install -y "gstreamer*"
-    yum install -y chrony
-    yum install -y kernel-devel kernel-headers gcc make perl
+    dnf install -y vim git wget ftp make cmake automake gcc gcc-c++ kernel-devel kernel-headers patch net-tools bind-utils epel-release asciidoc perl
+    dnf install -y xorg-x11-xinit xorg-x11-apps xorg-x11-xbitmaps xorg-x11-utils xterm xclip
+    dnf install -y xorg-x11-drv-evdev xorg-x11-drv-synaptics xorg-x11-fonts-misc.noarch libXrandr-devel libX11-devel libXft-devel libXScrnSaver-devel libcurl-devel libXt-devel
+    dnf install -y libXinerama-devel imsettings ncurses-term ncurses-devel imlib2-devel libexif-devel giflib-devel glm-devel glew-devel
+    dnf install -y gcr-devel mesa-libEGL mesa-libGL mesa-dri-drivers dbus-x11 dbus-devel
+    dnf install -y webkitgtk4-devel glib2-devel gcr-devel mesa-dri-drivers xorg-x11-utils libconfig-devel pango-devel gtk2-devel
+    dnf install -y "gstreamer*"
+    dnf install -y chrony
 }
 
 function Install_GoogleChrome() {
