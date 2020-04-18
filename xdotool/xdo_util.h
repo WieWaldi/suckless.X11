aa92f0fc23f062c7bf20c93e25d076d6dbb15ab8
Waldemar Schroeer <wiewaldi@idun.rz-amper.de>
2020-04-19 00:42:48 +0200

    xdotool added

---
+++ xdotool/xdo_util.h
@@ -0,0 +1,22 @@
+/* xdo utility pieces 
+ *
+ * $Id$
+ */
+
+#ifndef _XDO_UTIL_H_
+#define _XDO_UTIL_H_
+
+#include "xdo.h"
+
+/* human to Keysym string mapping */
+static const char *symbol_map[] = {
+  "alt", "Alt_L",
+  "ctrl", "Control_L",
+  "control", "Control_L",
+  "meta", "Meta_L",
+  "super", "Super_L",
+  "shift", "Shift_L",
+  NULL, NULL,
+};
+
+#endif /* ifndef _XDO_UTIL_H_ */
