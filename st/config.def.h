1ce1cc4ca1e327c354f47e6f3e8dfc8c055aabb6
Waldemar Schroeer <wiewaldi@idun.rz-amper.de>
2020-04-21 20:51:04 +0200

    rewritten and cleaned up

--- st/config.def.h
+++ st/config.def.h
@@ -110,6 +110,8 @@ static const char *colorname[] = {
 	/* more colors can be added after 255 to use with DefaultXX */
 	"#cccccc",
 	"#555555",
+    "#00ff00",
+    "#0000ff",
 };
 
 
@@ -117,10 +119,10 @@ static const char *colorname[] = {
  * Default colors (colorname index)
  * foreground, background, cursor, reverse cursor
  */
-unsigned int defaultfg = 11;
-unsigned int defaultbg = 15;
-static unsigned int defaultcs = 256;
-static unsigned int defaultrcs = 257;
+unsigned int defaultfg = 257;
+unsigned int defaultbg = 256;
+static unsigned int defaultcs = 258;
+static unsigned int defaultrcs = 259;
 
 /*
  * Default shape of cursor
@@ -156,36 +158,37 @@ static unsigned int defaultattr = 11;
  * Xresources preferences to load at startup
  */
 ResourcePref resources[] = {
-		{ "font",         STRING,  &font },
-		{ "color0",       STRING,  &colorname[0] },
-		{ "color1",       STRING,  &colorname[1] },
-		{ "color2",       STRING,  &colorname[2] },
-		{ "color3",       STRING,  &colorname[3] },
-		{ "color4",       STRING,  &colorname[4] },
-		{ "color5",       STRING,  &colorname[5] },
-		{ "color6",       STRING,  &colorname[6] },
-		{ "color7",       STRING,  &colorname[7] },
-		{ "color8",       STRING,  &colorname[8] },
-		{ "color9",       STRING,  &colorname[9] },
-		{ "color10",      STRING,  &colorname[10] },
-		{ "color11",      STRING,  &colorname[11] },
-		{ "color12",      STRING,  &colorname[12] },
-		{ "color13",      STRING,  &colorname[13] },
-		{ "color14",      STRING,  &colorname[14] },
-		{ "color15",      STRING,  &colorname[15] },
-		{ "background",   STRING,  &colorname[256] },
-		{ "foreground",   STRING,  &colorname[257] },
-		{ "cursorColor",  STRING,  &colorname[258] },
-		{ "termname",     STRING,  &termname },
-		{ "shell",        STRING,  &shell },
-		{ "xfps",         INTEGER, &xfps },
-		{ "actionfps",    INTEGER, &actionfps },
-		{ "blinktimeout", INTEGER, &blinktimeout },
-		{ "bellvolume",   INTEGER, &bellvolume },
-		{ "tabspaces",    INTEGER, &tabspaces },
-		{ "borderpx",     INTEGER, &borderpx },
-		{ "cwscale",      FLOAT,   &cwscale },
-		{ "chscale",      FLOAT,   &chscale },
+		{ "font",               STRING,  &font },
+		{ "color0",             STRING,  &colorname[0] },
+		{ "color1",             STRING,  &colorname[1] },
+		{ "color2",             STRING,  &colorname[2] },
+		{ "color3",             STRING,  &colorname[3] },
+		{ "color4",             STRING,  &colorname[4] },
+		{ "color5",             STRING,  &colorname[5] },
+		{ "color6",             STRING,  &colorname[6] },
+		{ "color7",             STRING,  &colorname[7] },
+		{ "color8",             STRING,  &colorname[8] },
+		{ "color9",             STRING,  &colorname[9] },
+		{ "color10",            STRING,  &colorname[10] },
+		{ "color11",            STRING,  &colorname[11] },
+		{ "color12",            STRING,  &colorname[12] },
+		{ "color13",            STRING,  &colorname[13] },
+		{ "color14",            STRING,  &colorname[14] },
+		{ "color15",            STRING,  &colorname[15] },
+		{ "background",         STRING,  &colorname[256] },
+		{ "foreground",         STRING,  &colorname[257] },
+		{ "cursorColor",        STRING,  &colorname[258] },
+		{ "cursorColorRev",     STRING,  &colorname[259] },
+		{ "termname",           STRING,  &termname },
+		{ "shell",              STRING,  &shell },
+		{ "xfps",               INTEGER, &xfps },
+		{ "actionfps",          INTEGER, &actionfps },
+		{ "blinktimeout",       INTEGER, &blinktimeout },
+		{ "bellvolume",         INTEGER, &bellvolume },
+		{ "tabspaces",          INTEGER, &tabspaces },
+		{ "borderpx",           INTEGER, &borderpx },
+		{ "cwscale",            FLOAT,   &cwscale },
+		{ "chscale",            FLOAT,   &chscale },
 };
 
 /*
