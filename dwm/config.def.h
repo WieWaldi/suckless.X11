a9175d027b2d629d530b78e6f5211109326afa5c
Waldemar Schroeer <w@ldemar.de>
2020-09-07 13:14:27 +0200

    dmenu for playing media

--- dwm/config.def.h
+++ dwm/config.def.h
@@ -112,7 +112,7 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[]           = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-p", "Яцп ТЋїѕ Ѕћїт:",  "-nb", col_DeepPink, "-nf", col_gray3, "-sb", col_DarkMagenta, "-sf", col_gray4, NULL };
 static const char *dmenusystem[]        = { "dwm_system", "-i", "-fn", dmenufont, "-p", "ЩЋдт тѳ dѳ", "-nb", col_DeepPink, "-nf", col_gray3, "-sb", col_DarkMagenta, "-sf", col_gray4, NULL };
-static const char *playradio[]          = { "play-radio", "-i", "-fn", dmenufont, "-p", "Play Radio", "-nb", col_DeepPink, "-nf", col_gray3, "-sb", col_DarkMagenta, "-sf", col_gray4, NULL };
+static const char *dmenumedia[]         = { "dwm_media", "-i", "-fn", dmenufont, "-p", "Play Radio", "-nb", col_DeepPink, "-nf", col_gray3, "-sb", col_DarkMagenta, "-sf", col_gray4, NULL };
 static const char *termcmd[]            = { "st", NULL };
 static const char *volumeup[]           = { "dwm_volumectrl", "up", NULL };
 static const char *volumedown[]         = { "dwm_volumectrl", "down", NULL };
@@ -125,7 +125,7 @@ static Key keys[] = {
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
 	{ MODKEY,                       XK_Return, spawn,          {.v = termcmd } },
 	{ MODKEY,                       XK_s,      spawn,          {.v = dmenusystem } },
-	{ MODKEY,                       XK_r,      spawn,          {.v = playradio } },
+	{ MODKEY,                       XK_r,      spawn,          {.v = dmenumedia } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
