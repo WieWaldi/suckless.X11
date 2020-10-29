fa9ab4aed9911d9c12d9aaf068f522388cf4cd75
Waldemar Schroeer <lassmichinruhe@rz-amper.de>
2020-10-29 22:39:10 +0100

    Minor changes.

--- prepare.sh
+++ prepare.sh
@@ -26,50 +26,52 @@ copy_Files () {
 }
 
 install_CentOS_7 () {
-    yum install -y vim git wget ftp make cmake automake gcc gcc-c++ kernel-devel kernel-headers patch net-tools bind-utils epel-release asciidoc perl htop >> prepare.log
-    yum install -y xorg-x11-xinit xorg-x11-apps xorg-x11-xbitmaps xorg-x11-utils xterm xclip sddm >> prepare.log
-    yum install -y xorg-x11-drv-evdev xorg-x11-drv-synaptics xorg-x11-fonts-misc.noarch libXrandr-devel libX11-devel libXft-devel libXScrnSaver-devel libcurl-devel libXt-devel libXtst-devel >> prepare.log
-    yum install -y libXinerama-devel imsettings ncurses-term ncurses-devel imlib2-devel libexif-devel giflib-devel glm-devel glew-devel libjpeg-turbo-devel >> prepare.log
-    yum install -y gcr-devel mesa-libEGL mesa-libGL mesa-dri-drivers dbus-x11 dbus-devel >> prepare.log
-    yum install -y webkitgtk4-devel glib2-devel gcr-devel mesa-dri-drivers xorg-x11-utils libconfig-devel pango-devel gtk2-devel >> prepare.log
-    yum install -y libdrm-devel libpciaccess-devel libxcb-devel jq fftw-devel iniparser libglvnd-devel elfutils-libelf-devel >> prepare.log
-    yum install -y alsa-utils alsa-lib-devel alsa-plugins-pulseaudio pulseaudio pulseaudio-libs-devel pulseaudio-libs-glib2 pulseaudio-module-x11 pulseaudio-utils >> prepare.log
-    yum install -y "gstreamer*" >> prepare.log
-    yum install -y ntp ntpdate >> prepare.log
+    echo -e "\nInstalling Repository: Extra Packages for Enterprise Linux 7"
+    yum install -y epel-release >> ${logfile} 2>&1
+    echo -e "\nInstalling Repository: RPM Fusion for EL 8 - Free - Updates"
+    yum localinstall -y --nogpgcheck https://mirrors.rpmfusion.org/free/el/rpmfusion-free-release-7.noarch.rpm >> ${logfile} 2>&1
+    echo -e "\nInstalling Repository: RPM Fusion for EL 8 - Nonfree - Updates"
+    yum localinstall -y --nogpgcheck https://mirrors.rpmfusion.org/nonfree/el/rpmfusion-nonfree-release-7.noarch.rpm >> ${logfile} 2>&1
+    IFS=$'\r\n' GLOBIGNORE='*' command eval  'packages=($(cat ./packages.CentOS8))'
+    echo -e "\nInstalling the following packages:"
+    echo ${packages[@]}
+    dnf install -y ${packages[@]} >> ${logfile} 2>&1
 }
 
 install_CentOS_8 () {
     echo -e "\nInstalling Repository: CentOS-8 - PowerTools"
-    dnf config-manager --enable PowerTools >> ${logfile}
+    dnf config-manager --enable PowerTools >> ${logfile} 2>&1
     echo -e "\nInstalling Repository: Extra Packages for Enterprise Linux 8"
-    dnf install -y epel-release >> ${logfile}
+    dnf install -y epel-release >> ${logfile} 2>&1
     echo -e "\nInstalling Repository: RPM Fusion for EL 8 - Free - Updates"
-    dnf install -y --nogpgcheck https://mirrors.rpmfusion.org/free/el/rpmfusion-free-release-8.noarch.rpm >> ${logfile}
+    dnf install -y --nogpgcheck https://mirrors.rpmfusion.org/free/el/rpmfusion-free-release-8.noarch.rpm >> ${logfile} 2>&1
     echo -e "\nInstalling Repository: RPM Fusion for EL 8 - Nonfree - Updates"
-    dnf install -y --nogpgcheck https://mirrors.rpmfusion.org/nonfree/el/rpmfusion-nonfree-release-8.noarch.rpm >> ${logfile}
-
+    dnf install -y --nogpgcheck https://mirrors.rpmfusion.org/nonfree/el/rpmfusion-nonfree-release-8.noarch.rpm >> ${logfile} 2>&1
     IFS=$'\r\n' GLOBIGNORE='*' command eval  'packages=($(cat ./packages.CentOS8))'
     echo -e "\nInstalling the following packages:"
     echo ${packages[@]}
-    dnf install -y ${packages[@]} >> ${logfile}
+    dnf install -y ${packages[@]} >> ${logfile} 2>&1
 }
 
 install_GoogleChrome() {
     echo -e "\nInstalling Repository: google-chrome"
     cp ${cdir}/etc/yum.repos.d/google-chrome.repo /etc/yum.repos.d
-    yum install -y google-chrome-stable >> ${logfile}
+    yum install -y google-chrome-stable >> ${logfile} 2>&1
 }
 
 install_VirtualBox() {
     echo -e "\nInstalling Repository: VirtualBox"
     cp ${cdir}/etc/yum.repos.d/virtualbox.repo /etc/yum.repos.d
-    yum install -y VirtualBox-6.0 >> ${logfile}
+    yum install -y VirtualBox-6.0 >> ${logfile} 2>&1
 }
 
 if ! [[ $(/bin/id -u) = 0 ]]; then
     printf "\n This script must be run as root.\n\n" 
     exit 1
 else
+    if [[ -f ${logfile} ]]; then
+        rm ${logfile}
+    fi
     if [[ -f /etc/os-release ]]; then
         . /etc/os-release
         os=$NAME
