03412e3634d89653ad2df463d5f927e553017a46
Waldemar Schroeer <wiewaldi@fenrjer.rz-amper.de>
2021-04-29 23:53:17 +0200

    Use scroll wheel to zoom.

--- st/config.def.h
+++ st/config.def.h
@@ -227,15 +227,17 @@ ResourcePref resources[] = {
  * Beware that overloading Button1 will disable the selection.
  */
 static MouseShortcut mshortcuts[] = {
-	/* mask                 button   function        argument       release */
-	{ ShiftMask,            Button4, kscrollup,      {.i = 5} },
-	{ ShiftMask,            Button5, kscrolldown,    {.i = 5} },
-	{ XK_ANY_MOD,           Button2, selpaste,       {.i = 0},      1 },
-	{ XK_ANY_MOD,           Button3, clippaste,      {.i = 0},      1 },
-	{ ShiftMask,            Button4, ttysend,        {.s = "\033[5;2~"} },
-	{ XK_ANY_MOD,           Button4, ttysend,        {.s = "\031"} },
-	{ ShiftMask,            Button5, ttysend,        {.s = "\033[6;2~"} },
-	{ XK_ANY_MOD,           Button5, ttysend,        {.s = "\005"} },
+	/* mask                 button          function        argument       release */
+	{ ControlMask,          Button4,        zoom,           {.f = +1} },
+	{ ControlMask,          Button5,        zoom,           {.f = -1} },
+	{ ShiftMask,            Button4,        kscrollup,      {.i = 5} },
+	{ ShiftMask,            Button5,        kscrolldown,    {.i = 5} },
+	{ XK_ANY_MOD,           Button2,        selpaste,       {.i = 0},      1 },
+	{ XK_ANY_MOD,           Button3,        clippaste,      {.i = 0},      1 },
+	{ ShiftMask,            Button4,        ttysend,        {.s = "\033[5;2~"} },
+	{ XK_ANY_MOD,           Button4,        ttysend,        {.s = "\031"} },
+	{ ShiftMask,            Button5,        ttysend,        {.s = "\033[6;2~"} },
+	{ XK_ANY_MOD,           Button5,        ttysend,        {.s = "\005"} },
 };
 
 /* Internal keyboard shortcuts. */
