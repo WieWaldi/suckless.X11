8d0ddccb5b58e736591761127919377372ca1a63
Waldemar Schroeer <w@ldemar.de>
2022-07-23 23:03:14 +0200

    Minor changes.

--- dwm-helper/autostart.sh
+++ dwm-helper/autostart.sh
@@ -10,14 +10,20 @@
 # +----- Autostart ------------------------------------------------------------+
 # /bin/xsetroot -fg black -bg white -bitmap /usr/include/X11/bitmaps/root_weave
 
+
 /usr/local/bin/dunst & > /dev/null 2>&1 &
-amixer -q -D pulse sset Master 25%
 ${HOME}/.local/bin/xclickroot -r ${HOME}/.local/bin/xmenu.sh &
-/bin/dbus-launch ${HOME}/.local/bin/picom --experimental-backends &
+/bin/dbus-launch /bin/picom --experimental-backends &
 ${HOME}/.local/bin/dwm-status &
 ${HOME}/.local/bin/rotwall ${HOME}/.local/share/wallpapers 300 &
 
 ${HOME}/.local/bin/st &
-${HOME}/.local/bin/st &
+${HOME}/.local/bin/st -e ${HOME}/.local/bin/tmux-start.sh Login &
+
+# /usr/bin/oneko -name "oneko" -tofocus -sakura -bg pink -speed 32 -time 75000 &
+/bin/google-chrome &
+
+/usr/bin/amixer -q -D pulse sset Master 45% &
+/bin/mplayer ~/.local/share/sounds/Windows/Windows95_Login.ogg > /dev/null 2>&1
 
 exit 0
