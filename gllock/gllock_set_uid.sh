57c1843bf7a485ae1d727bb4d844ccfc64fc7a86
Waldemar Schroeer <w@ldemar.de>
2022-07-26 09:19:17 +0200

    Add script to set uid.

---
+++ gllock/gllock_set_uid.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+#
+# +----------------------------------------------------------------------------+
+# | gllock_set_uid.sh                                                          |
+# +----------------------------------------------------------------------------+
+# | Copyright © 2019 Waldemar Schroeer                                         |
+# |                  waldemar.schroeer(at)rz-amper.de                          |
+# +----------------------------------------------------------------------------+
+
+sudo chown root:root ~/.local/bin/gllock
+sudo chmod u+s ~/.local/bin/gllock
+
+exit 0
