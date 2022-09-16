4b8bb47f250590501458eb45549230017d45e615
Waldemar Schroeer <w@ldemar.de>
2022-09-16 22:30:00 +0200

    Minor changes

--- xmenu/xmenu.sh
+++ xmenu/xmenu.sh
@@ -29,6 +29,7 @@ Office
 	IMG:${HOME}/.local/share/icons/hicolor/48x48/apps/rz-libreoffice-math.png		LibreOffice Math			/bin/libreoffice --math
 	IMG:${HOME}/.local/share/icons/hicolor/48x48/apps/rz-libreoffice-writer.png		LibreOffice Writer			/bin/libreoffice --writer
 System Application
+	IMG:${HOME}/.local/share/icons/hicolor/48x48/apps/rz-gear.png					Arandr						/usr/bin/arandr
 	IMG:${HOME}/.local/share/icons/hicolor/48x48/apps/rz-compton.png				Compton (start)				/bin/dbus-launch ${HOME}/.local/bin/compton
 	IMG:${HOME}/.local/share/icons/hicolor/48x48/apps/rz-compton.png				Compton (kill)				/bin/pkill compton
 	IMG:${HOME}/.local/share/icons/hicolor/48x48/apps/rz-compton.png				Picom (start)				/usr/bin/picom --experimental-backends
